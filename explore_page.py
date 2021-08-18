3cc7d20e87dd3d1a51a1c2eecf932f72559e7bb8
NXture <varunvaru27101999@gmail.com>
2021-08-19 00:48:14 +0530

    chaged code from double quote to single quote😑

--- explore_page.py
+++ explore_page.py
@@ -33,22 +33,22 @@ def clean_education(x):
 
 @st.cache
 def load_data():
-    df = pd.read_csv("survey_results_public.csv")
-    df = df[["Country","EdLevel","YearsCodePro","Employment","ConvertedComp"]]
-    df = df[df["ConvertedComp"].notnull()]
+    df = pd.read_csv('survey_results_public.csv')
+    df = df[['Country','EdLevel','YearsCodePro','Employment','ConvertedComp']]
+    df = df[df['ConvertedComp'].notnull()]
     df = df.dropna()
-    df = df[df["Employment"] == "Employed full-time"]
-    df = df.drop("Employment", axis=1)
+    df = df[df['Employment'] == 'Employed full-time']
+    df = df.drop('Employment', axis=1)
 
     country_map = shorten_categories(df.Country.value_counts(), 400)
-    df["Country"] = df["Country"].map(country_map)
-    df = df[df["ConvertedComp"] <= 250000]
-    df = df[df["ConvertedComp"] >= 10000]
-    df = df[df["Country"] != "Other"]
+    df['Country'] = df['Country'].map(country_map)
+    df = df[df['ConvertedComp'] <= 250000]
+    df = df[df['ConvertedComp'] >= 10000]
+    df = df[df['Country'] != 'Other']
 
-    df["YearsCodePro"] = df["YearsCodePro"].apply(clean_experience)
-    df["EdLevel"] = df["EdLevel"].apply(clean_education)
-    df = df.rename({"ConvertedComp": "Salary"}, axis=1)
+    df['YearsCodePro'] = df['YearsCodePro'].apply(clean_experience)
+    df['EdLevel'] = df['EdLevel'].apply(clean_education)
+    df = df.rename({'ConvertedComp': 'Salary'}, axis=1)
     return df
 
 
@@ -64,13 +64,13 @@ def show_explore_page():
     """
     )
 
-    data = df["Country"].value_counts()
+    data = df['Country'].value_counts()
 
     fig1, ax1 = plt.subplots()
     ax1.pie(data, labels=data.index, autopct="%1.1f%%",
             shadow=True, startangle=90)
     # Equal aspect ratio ensures that pie is drawn as a circle.
-    ax1.axis("equal")
+    ax1.axis('equal')
 
     st.write("""#### Number of Data from different countries""")
 
@@ -82,7 +82,7 @@ def show_explore_page():
     """
     )
 
-    data = df.groupby(["Country"])["Salary"].mean().sort_values(ascending=True)
+    data = df.groupby(['Country'])['Salary'].mean().sort_values(ascending=True)
     st.bar_chart(data)
 
     st.write(
@@ -91,6 +91,6 @@ def show_explore_page():
     """
     )
 
-    data = df.groupby(["YearsCodePro"])[
-        "Salary"].mean().sort_values(ascending=True)
+    data = df.groupby(['YearsCodePro'])[
+        'Salary'].mean().sort_values(ascending=True)
     st.line_chart(data)
